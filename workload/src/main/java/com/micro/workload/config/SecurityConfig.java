3e44464a5187b15d1ae0cb87ad002a7f5dad3861
StarFleet1334 <latariailia6@gmail.com>
2025-01-06 23:35:24 +0400

    update code vol2

--- workload/src/main/java/com/micro/workload/config/SecurityConfig.java
+++ workload/src/main/java/com/micro/workload/config/SecurityConfig.java
@@ -6,22 +6,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
+    public SecurityFilterChain testSecurityChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/trainings/**").authenticated()
-                        .anyRequest().permitAll())
-                .addFilterBefore(new JwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+                        .anyRequest().permitAll());
         return http.build();
     }
+
 
 
 
